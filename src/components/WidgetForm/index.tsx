c585568f992f0ef8696d1f70c340d15c97045aef
antonioFilho-global <antonio.afce@gmail.com>
2022-08-03 11:31:14 -0300

    Melhorando os componentes do form do feed back

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,12 +1,16 @@
-import { CloseButton } from "./CloseButton";
+import { useState } from "react";
+
+import { CloseButton } from "../CloseButton";
 
 /* importando imagens dos icones */
-import bugImageURL from '../assets/bug.svg';
-import ideaImageURL from '../assets/idea.svg';
-import thoughtImageURL from '../assets/thought.svg';
+import bugImageURL from '../../assets/bug.svg';
+import ideaImageURL from '../../assets/idea.svg';
+import thoughtImageURL from '../../assets/thought.svg';
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
 /* Criando objeto que contem as categorias de feedbacks */
-const feedbackType = {
+export const feedbackTypes = {
     BUG: {
         title: 'Problema',
         image: {
@@ -30,6 +34,9 @@ const feedbackType = {
     },
 };
 
+/* export type FeedbackType = keyof typeof feedbackTypes; */
+export type FeedbackType = keyof typeof feedbackTypes;
+
 /* // Object.entries(feedbackType) =>
 Salvando em forma de vetor, que vai ser percorrido com um .map
 [ 
@@ -39,32 +46,25 @@ Salvando em forma de vetor, que vai ser percorrido com um .map
 ] */
 
 export function WidgetForm() {
+    /* Salvando como state a categoria de feedback selecionada pelo usuário */ 
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
     return (
         /*   A biblioteca é util para fazer a recursividade de maneira prática: w-[calc(100vw-2rem)] md:w-auto */
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
                 <span className="text-xl leading-6">Deixe seu feedback</span>
 
-               {/*  Nao esta funcionado */}
                 <CloseButton /> 
             </header>
-            
-            <div className="flex py-8 gap-2 w-full">
-                { Object.entries(feedbackType).map(([key, value]) =>{
-                    return(
-                        <button
-                            key={key}
-                            className="br-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 
-                                       border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                            /* onClick{} */
-                            type="button"
-                        >
-                            <img src={value.image.source} alt={value.image.alt} />
-                            <span>{value.title}</span>
-                        </button>
-                    )
-                }) }
-            </div>
+
+            {/* Selecionando o tipo de feedback */}
+            {!feedbackType ? (
+               /*  Eviando função de um componente pai para um componente filho fia propriedade*/
+                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            ) : (
+                <p>Hello World</p>
+            )}
             
 
             <footer className="text-xs text-neutral-400">
