49fc29cc96bcc63d11a1942c0725cf5a2c20c5f2
antonioFilho-global <antonio.afce@gmail.com>
2022-08-16 17:23:31 -0300

    Implementação de envio de emails via API, contendo o feedback. Usando o nodemailer e a plataforma mailtrap

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express' 
+import nodemailer from 'nodemailer'
 import { prisma } from './prisma';
 
 /* Iniciando o express */
@@ -15,6 +16,15 @@ app.use(express.json());
 // PATCH = Atualizar uma informação única de uma entidade
 // DELETE = Deletar uma Informação
 
+/* Configurando o nodemailer, para enviar o feedback por email */
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "a2b5c92a75c014",
+      pass: "39cb098df52bf4"
+    }
+  });
 
 
 /* Rota cadastro de feedbacks*/
@@ -29,6 +39,20 @@ app.post('/feedbacks', async(req, res) => {
         }
     })
 
+    /* Usando o nodemailer com o mailtrap para enviar emails */
+    transport.sendMail({
+        /* Especificando para qual email vai ser mandado e o seu assunto */ 
+        from: 'Equipe Feedget <oi@feedget.com>',
+        to: 'Antonio Filho <antonio.afce@gmail.com>',
+        subject: 'Novo feedback',
+        /* formatando o conteúdo do email */
+        html: [
+            `<div style="font-family: sans-serif; font-size:16px; color: #111;">`,
+            `<p>Tipo de feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`,
+            `</div>`
+        ].join('\n')
+    });
     /* Status http de criação 201 */
     /* mandando os dados via JSON */
     return res.status(201).json({ data: feedback });
